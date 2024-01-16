2b22361676007b2513c61090fcc0625f3c9ceda6
Tina Ostrander <tostrander@greenriver.edu>
2024-01-16 13:01:44 -0500

    Controller created

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-echo "My Diner";
+/* Tina Ostrander
+ * January 2024
+ * https://tostrander.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+// Instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); //static method
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo "My Diner";
+});
+
+// Run Fat-Free
+$f3->run(); //instance method
